8c8b221a3e7a0e3d1f5627ace6e0e5a11680a663
Gabriel <gmonteiro810@gmail.com>
2023-12-21 14:35:46 -0300

    new

--- create-db-template.sql
+++ create-db-template.sql
@@ -15,6 +15,17 @@ SHOW TABLES;
 
 DESC cliente;
 
+INSERT INTO cliente
+VALUES
+("Ronaldo",'M',"R9@g1alactico.com","01234567891","(44) 1123456789","Casa1"),
+("Ronaldo2",'M',"R9@g2alactico.com","01234567891","(44) 1123456789","Casa2"),
+("Ronaldo3",'M',"R9@3galactico.com","01234567891","(44) 1123456789","Csa3"),
+("Ronaldo4",'M',"R9@4galactico.com","01234567891","(44) 1123456789","Casa4");
+
+/*Comandos Select
+Seleção projeção e junção*/
+
+
 
 
 
